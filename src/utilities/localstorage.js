ff383640664e83eec5bba4b3c06ee950dbb09108
AdnaNation <azizulislamadnan@gmail.com>
2024-03-13 06:55:02 +0600

    a commit

---
+++ src/utilities/localstorage.js
@@ -0,0 +1,24 @@
+const getStoredCart =() =>{
+  const storedCarString =   localStorage.getItem('cart');
+  if(storedCarString){
+    return JSON.parse(storedCarString)
+  }
+  return [];
+}
+const saveCartToLS = cart =>{
+    const cartStringified = JSON.stringify(cart);
+    localStorage.setItem('cart', cartStringified)
+}
+const addToLS = id =>{
+    const cart = getStoredCart();
+    cart.push(id);
+    // save to local storage
+    saveCartToLS(cart);
+}
+const removeFromLS = id =>{
+  const cart = getStoredCart();
+  const remaining = cart.filter(idx => idx !== id);
+  saveCartToLS(remaining);
+}
+export { addToLS, getStoredCart, removeFromLS };
+
